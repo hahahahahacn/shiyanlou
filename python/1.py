351a378d1b02154228ce65ccafe28f119982e0fd
hahahahahacn <sjlhcn@qq.com>
2018-07-05 17:41:50 +0800

    添加python程序

---
+++ python/1.py
@@ -0,0 +1,11 @@
+# This program sys hello and asks for my name.
+
+print('Hello World!')
+print('what is your name?')		#ask for their name
+myname = input()
+print('It is good to meet you, ' + myname)
+pritn('the length of your name is:')
+print(len(myname))
+print('What is your age?')	#ask for their age
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year.')
