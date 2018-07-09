5d1fc75a6a6ec39844b276294dd70b6f5a1b20d6
hahahahahacn <sjlhcn@qq.com>
2018-07-09 15:00:30 +0800

    15:00

---
+++ python/hello.py
@@ -0,0 +1,12 @@
+print('请输入变量a： ')
+a = int(input())
+print()
+
+if a > 10:
+    print('a>10, a = ', str(a))
+else:
+    print('a<=10, a = ', str(a))
+
+print('good bye.  ')
+a = str(a)
+print(len(a))
